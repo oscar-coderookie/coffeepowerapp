c9930616b49781584666c23f660a7e4f6e6b9ffe
unknown <industrialdesignoscar@gmail.com>
2025-09-17 12:38:54 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+const OurCoffees = () => {
+  return (
+    <View style={styles.sections}>
+      <Text style={{ color: '#1D1E20' }}>Nuestros Cafés</Text>
+    </View>
+  )
+};
+const QuienSomos = () => {
+  return (
+    <View style={styles.sections}>
+      <Text style={{ color: '#1D1E20' }}>¿Quiénes somos?</Text>
+    </View>
+  )
+};
+const Contacto = () => {
+  return (
+    <View style={styles.sections}>
+      <Text style={{ color: '#1D1E20' }}>Contacto</Text>
+    </View>
+  )
+};
+const AccesoriosPro = () => {
+  return (
+    <View style={styles.sections}>
+      <Text style={{ color: '#1D1E20' }}>Accesorios Pro</Text>
+    </View>
+  )
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarIcon: () => null,
+          tabBarShowLabel: true, // 👈 muestra solo el texto
+           tabBarLabelStyle: {
+            fontSize: 12,
+            lineHeight: 16,
+            width: 80,
+            height: '100%'// 👈 sube el texto para centrarlo
+          },
+          tabBarStyle: {
+            backgroundColor: "#000000",
+            height: 100,
+            // 👈 ajusta altura para que quede bonito
+          },
+          tabBarActiveTintColor: '#b6a963ff',
+          tabBarInactiveTintColor: '#fff',
+
+
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Nuestros Cafés" component={OurCoffees} />
+        <Tab.Screen name="¿Quiénes Somos?" component={AccesoriosPro} />
+        <Tab.Screen name="Contacto" component={Contacto} />
+        <Tab.Screen name="Accesorios Pro" component={AccesoriosPro} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  sections: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
+
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { Video } from 'expo-av';
+import fondo from '../assets/home-movil.mp4';
+import logo from '../assets/logo-nuevo.png'
+
+const { width, height } = Dimensions.get('window');
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      {/* Video de fondo */}
+      <Video
+        source={fondo} // 👈 aquí va tu video (puede ser local o url)
+        style={styles.backgroundVideo}
+        resizeMode="contain"
+        shouldPlay
+        isLooping
+        isMuted
+      />
+
+      {/* Contenido encima del video */}
+      <View style={styles.overlay}>
+        <Image source={logo} style={{width: 200 , height: 200}}/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width,
+    height: height,
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)', // opcional: oscurece el video
+  },
+  title: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
